f7b894896bd5a0249203d9a70904f5ec7dd6b4e0
Alex Gonzalez <alexg@Alex-944.local>
2024-09-23 15:01:24 -0700

    remove all scrappers besides the nba one

--- PrizePicks/backend/prizepicks/prizepicks/settings.py
+++ PrizePicks/backend/prizepicks/prizepicks/settings.py
@@ -74,10 +74,13 @@ WSGI_APPLICATION = "prizepicks.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
 }
 
 
