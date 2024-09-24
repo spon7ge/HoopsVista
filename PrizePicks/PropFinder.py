4ee339845f663cb890fd21c8cb343cdb07f2afd3
Alejandro Gonzalez <alexgonzo33@gmail.com>
2024-09-23 23:36:01 -0700

    removed csv_files from here

--- PrizePicks/PropFinder.py
+++ PrizePicks/PropFinder.py
@@ -1,17 +1,8 @@
 from NBAPropFinder.NBAPropFinder import NBAPropFinder
-from NFLPropFinder.NFLPropFinder import NFLPropFinder
-from NHLPropFinder.NHLPropFinder import NHLPropFinder
-from NCAABPropFinder.NCAABPropFinder import NCAABPropFinder
-from MLBPropFinder.MLBPropFinder import MLBPropFinder
-from WNBAPropFinder.WNBAPropFinder import WNBAPropFinder
 
 """
 Reccomended to run one prop finder at a time by commenting all the other ones.
 """
 
-#nba_props = NBAPropFinder()
-#nhl_props = NHLPropFinder()
-nfl_props = NFLPropFinder()
-#ncaab_props = NCAABPropFinder()
-#mlb_props = MLBPropFinder()
-#wnba_props = WNBAPropFinder()
+nba_props = NBAPropFinder()
+
