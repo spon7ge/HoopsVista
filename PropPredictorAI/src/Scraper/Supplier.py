7013dd4a9e2a6532956c09ddf2022b90e6ee15cd
Alejandro Gonzalez <alexgonzo33@gmail.com>
2024-10-21 22:59:46 -0700

    put api key inside .env file

--- PropPredictorAI/src/Scraper/Supplier.py
+++ PropPredictorAI/src/Scraper/Supplier.py
@@ -1,11 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Supplier:
     def __init__(self):
         # The key is the API key for the-odds-api at https://the-odds-api.com/#get-access
-        self.key = "14ac393951a4b6e8b75ca4a7073db8a1"
+        self.key = os.getenv('api_key')
         
         # The directory is the location of the projections.json file
         # self.directory = "/Users/alexg/Downloads/projections.json"
         self.directory = "c:/Users/alexg/Downloads/projections.json"
+
     def get_key(self):
         return self.key
     
